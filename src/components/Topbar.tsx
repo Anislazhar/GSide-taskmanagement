2e117f6054655590b9d599bd0ffa8a834e831f27
Anis Lazhar <kateryna@Anis-MacBook-Pro.local>
2025-10-17 12:52:03 +0200

    created topbar

---
+++ src/components/Topbar.tsx
@@ -0,0 +1,21 @@
+import { ClipboardCheck } from "lucide-react";
+import React from "react";
+
+export default function Topbar() {
+  return (
+    <header>
+      <div className="max-w-[1200px] mx-auto md:ml-20 py-4 flex  ">
+        <div className="flex items-center gap-3 mb-1 pr-4">
+          <ClipboardCheck size={20} />
+        </div>
+        <div>
+          <div className="text text-slate-800 font-bold">Alex Azubi</div>
+
+          <div className="flex items-center gap-3">
+            <div className="text-sky-600">Taskmanager</div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
